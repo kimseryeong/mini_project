498f77c6aab4a5774d198cf69853dc5dc3e394a3
kimseryeong <kxxoo322@gmail.xom>
2024-03-09 21:34:55 +0900

    기능 삽입

---
+++ todoList/main.js
@@ -0,0 +1,44 @@
+//할 일 목록 추가하기
+var cnt = 0;
+function fnAddTodo(){
+    cnt++;
+    var addTodoInput = $('#addTodoInput').val();
+    console.log('버튼 클릭됨 >> ', cnt);
+
+    if(addTodoInput == "" || addTodoInput == null){
+        alert("입력값 없음");
+        return false;
+    }
+    var list = '<div class="cont">'
+                + '<input type="checkbox" id="chkbox' + cnt + '" name="chkbox" onclick="fnChkTodo(event)">'
+                + '<label for="chkbox' + cnt + '" class="todo_chk" ></label>'
+                + '<span class="todo_cont">'+ addTodoInput + '</span>'
+                + '<button onclick="fnDelTodo(event)" type="button" class="del_todo_btn"><img src="./img/close.png" alt="X"></button>'
+                + '</div>';
+
+    $('.input-div').append(list);
+    $('#addTodoInput').val("");
+}
+
+//할 일 목록 완료 체크
+function fnChkTodo(event) {
+    var checkbox = event.target;
+    var span = checkbox.nextElementSibling.nextElementSibling; // 다음 두 번째 sibling을 찾음
+
+    if (checkbox.checked) {
+        //console.log('체크됨');
+        span.style.color = '#9ac57b';
+        span.style.textDecoration = 'line-through';
+    } else {
+        //console.log('체크 해제됨');
+        span.style.color = ''; // 기본값으로 설정
+        span.style.textDecoration = ''; // 기본값으로 설정
+    }
+}
+
+//할 일 목록 삭제
+function fnDelTodo(event){
+    var delBtn = event.target;
+    var div = delBtn.closest('div');
+    div.remove();
+}
