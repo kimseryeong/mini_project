1f4635066deb880dc7970c39a5250323665bc648
kimseryeong <kxxoo322@gmail.com>
2024-03-11 17:54:08 +0900

    0311

--- calculator/algorithm/main.js
+++ calculator/algorithm/main.js
@@ -1,7 +1,20 @@
+// ******* 계산기 사칙연산 로직 생각해보기 *******
+
+/*
+입력값(숫자) + 연산자 + 입력값(숫자) + '=' ----> 결과값 반환
+입력값을 쌓으면서 만약 연산자가 들어온다면, 
+입력값 쌓아놓은 숫자를 firstNum에 담고
+연산자를 담고
+다시 '='이 나올때까지 숫자 담고
+
+
+*/
+
 var txt = '';
 $('.btn').on('click', function(){
     
     var className = $(this).attr('class');
+    var inputTxt = $(this).text();
     
     //AC 클릭 시 초기화
     if(className.indexOf('clear') > -1){
@@ -10,10 +23,16 @@ $('.btn').on('click', function(){
         return false;
     }
 
-    txt += $(this).text();
-    console.log(txt);
+    firsttxt += $(this).text();
+    console.log(className, txt);
+    
+    if(inputTxt == '*'){
+
+    }
+    
     $('.inout').html(txt);
 
+    
 
     var res = 0;
 
@@ -22,3 +41,4 @@ $('.btn').on('click', function(){
 function fnCalc(){
     
 }
+
